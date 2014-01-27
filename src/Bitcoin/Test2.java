4335cc1e1db431d33f81192d9a6f81fde8ca23f1
Tchak <tchakmakdjian.kevin@gmail.com>
2014-01-27 17:41:26 +0100

    azerty

--- src/Bitcoin/Test2.java
+++ src/Bitcoin/Test2.java
@@ -2,4 +2,8 @@ package Bitcoin;
 
 public class Test2 {
 
+	public void bla(){
+		System.out.println("Si tu as l'impression de n'etre qu'un raté, dis toi que le grand chene était par le passé qu'un gland lui aussi");
+	
+	}
 }
