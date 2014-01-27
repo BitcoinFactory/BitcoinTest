e3a23d33131e4f2271c0bafa9bb05969aa8bc551
Ar4kawa <stefan.angoso@gmail.com>
2014-01-27 19:23:56 +0100

    clientv1

--- src/Bitcoin/Client.java
+++ src/Bitcoin/Client.java
@@ -1,5 +1,40 @@
 package Bitcoin;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
 
+	public static void main (String[] args) throws IOException{
+	
+		int port = 2000;
+		InetAddress ip = InetAddress.getLocalHost();
+		
+		try{
+			Socket sClient = new Socket(ip, port);						// On ouvre une connexion du client au serveur
+			
+			System.out.println("ip");
+			
+			int i;
+			
+			for (i = 0 ; i < 10000 ; i++){								// Le client envoie des valeurs en continu au serveur
+				String str = Integer.toString(i);
+				
+				sClient.getOutputStream().write(str.getBytes());  		// On envoie le nombre sous forme de char
+			}
+			
+			sClient.close();
+		}
+		catch(IOException e){
+			System.out.println("Vous ne pouvez pas vous connecter");
+		}
+		
+		
+		
+	}
+	
+
+	
+	
 }
