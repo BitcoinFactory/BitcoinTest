d915cbe657f7d78bc6b99af2cc5d0293c468955f
Tchak <tchakmakdjian.kevin@gmail.com>
2014-01-27 18:14:13 +0100

    12321

--- src/Bitcoin/BlaTest.java
+++ src/Bitcoin/BlaTest.java
@@ -4,6 +4,6 @@ public class BlaTest {
 		
 	
 	public static void main (String[] args){
-		System.out.println("Qui mange des noix de coco, fait confiance à son an**");
+		System.out.println("Qui mange des noix de coco, fait confiance à son anus");
 	}
 }
